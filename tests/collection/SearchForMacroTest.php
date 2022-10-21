9069e1d9f0f55eda873b91ee261a2465beabbafb
m-alghobary <m.alghobary90@gmail.com>
2022-10-21 11:21:03 +0300

    refactore and orginaize the test files

--- tests/collection/SearchForMacroTest.php
+++ tests/collection/SearchForMacroTest.php
@@ -1,21 +1,8 @@
 <?php
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 use function PHPUnit\Framework\assertEquals;
-
-it('can paginate collections', function () {
-    $items = collect(range(1, 100));
-
-    $paginated = $items->paginate(10);
-
-    assert($paginated instanceof LengthAwarePaginator);
-    assertEquals(count($paginated->toArray()['data']), 10);
-    assertEquals($paginated->toArray()['per_page'], 10);
-    assertEquals($paginated->toArray()['total'], 100);
-});
-
 
 it('can search in collection of objects', function () {
 
