4a091f21643205d8795e06e84cc99e72a86531a7
m-alghobary <m.alghobary90@gmail.com>
2022-10-22 20:39:54 +0300

    add test case for database query builder in the getSql macro tests

--- tests/builder/GetSqlMacroTest.php
+++ tests/builder/GetSqlMacroTest.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace Alghobary\Tests\Builder;
+namespace Alghobary\Tests\Builder\GetSql;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 use function PHPUnit\Framework\assertIsString;
 use function PHPUnit\Framework\assertEqualsIgnoringCase;
 use function PHPUnit\Framework\assertStringNotContainsString;
 
-it('should get the builder sql query with the actual values', function () {
+
+it('should get the eloquent builder sql query with the actual values', function () {
     class MyModel extends Model
     {
     }
@@ -22,4 +24,18 @@ it('should get the builder sql query with the actual values', function () {
     assertIsString($result);
     assertStringNotContainsString('?', $result);
     assertEqualsIgnoringCase($result, "select * from `my_models` where `id` = '1' or `name` LIKE '%Ali%' order by `created_at` desc");
+});
+
+
+it('should get the query builder sql query with the actual values', function () {
+
+    $result = DB::table('users')
+        ->where('id', 1)
+        ->orWhere('name', 'LIKE', '%Ali%')
+        ->latest()
+        ->getSql();
+
+    assertIsString($result);
+    assertStringNotContainsString('?', $result);
+    assertEqualsIgnoringCase($result, "select * from `users` where `id` = '1' or `name` LIKE '%Ali%' order by `created_at` desc");
 });
