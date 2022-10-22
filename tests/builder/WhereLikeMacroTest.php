346ca84137f8df1fbe611b91fe697ae22eaaf29c
m-alghobary <m.alghobary90@gmail.com>
2022-10-22 20:39:16 +0300

    add builder whereLike macro and test it

---
+++ tests/builder/WhereLikeMacroTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Alghobary\Tests\Builder\WhereLike;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+use function PHPUnit\Framework\assertIsString;
+use function PHPUnit\Framework\assertStringContainsStringIgnoringCase;
+
+
+it('should add the right sql where LIKE conditions to the eloquent query builder', function () {
+    class MyModel extends Model
+    {
+    }
+
+    $result = MyModel::query()
+        ->where('id', 1)
+        ->whereLike(['name', 'email'], 'ali')
+        ->getSql();
+
+    assertIsString($result);
+    assertStringContainsStringIgnoringCase("and (`name` LIKE '%ali%' or `email` LIKE '%ali%')", $result);
+});
+
+
+it('should add the right sql where LIKE conditions to the database query builder', function () {
+
+    $result = DB::table('users')
+        ->where('id', 1)
+        ->whereLike(['name', 'email'], 'ali')
+        ->getSql();
+
+    assertIsString($result);
+    assertStringContainsStringIgnoringCase("and (`name` LIKE '%ali%' or `email` LIKE '%ali%')", $result);
+});
